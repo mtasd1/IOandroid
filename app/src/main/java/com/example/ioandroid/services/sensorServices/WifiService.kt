8a576c726502a01c305e273f7a2452f7f729a1b4
mtasd1 <m.tasdemir2000@hotmail.com>
2024-06-13 11:28:19 +0200

    refactor permission handling in WifiService and replace lstm classiier

--- app/src/main/java/com/example/ioandroid/services/sensorServices/WifiService.kt
+++ app/src/main/java/com/example/ioandroid/services/sensorServices/WifiService.kt
@@ -13,34 +13,6 @@ import org.json.JSONObject
 class WifiService(private val context: Context) {
     private val wifiManager: WifiManager = context.applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
 
-    fun getWifiNetworks(): List<ScanResult> {
-        if (ActivityCompat.checkSelfPermission(
-                context,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            ActivityCompat.requestPermissions(
-                context as CollectActivity,
-                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                1
-            )
-        }
-
-        if (ActivityCompat.checkSelfPermission(
-                context,
-                Manifest.permission.ACCESS_WIFI_STATE
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            ActivityCompat.requestPermissions(
-                context as CollectActivity,
-                arrayOf(Manifest.permission.ACCESS_WIFI_STATE),
-                1
-            )
-        }
-
-        return wifiManager.scanResults
-    }
-
     fun getWifiNetworksJSON(): JSONArray {
         val wifiNetworksJSON = JSONArray()
         val wifiNetworks = getWifiNetworks()
@@ -53,9 +25,30 @@ class WifiService(private val context: Context) {
         return wifiNetworksJSON
     }
 
+    private fun getWifiNetworks(): List<ScanResult> {
+        ensurePermissions()
+        return wifiManager.scanResults
+    }
+
     fun enableWifi() {
         if (!wifiManager.isWifiEnabled) {
             wifiManager.isWifiEnabled = true
+        }
+    }
+
+    private fun ensurePermissions() {
+        val permissions =
+            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_WIFI_STATE)
+        if (ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED ||
+            ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.ACCESS_WIFI_STATE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(context as CollectActivity, permissions, 1)
         }
     }
 }
